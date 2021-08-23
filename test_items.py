bba011b56a5f5b15a1514e56b38d6eba198ea124
Пронина Елена <e.pronina@tendertech.ru>
2021-08-23 13:30:31 +0300

    first commit

---
+++ test_items.py
@@ -0,0 +1,12 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
+
+    time.sleep(15)
+
+
+
